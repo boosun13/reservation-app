195d94a00e5cf4850f6f5ca03d6b0296b916d9a3
boosun13 <double0two22@gmail.com>
2021-03-23 01:33:28 +0900

    フロントエンドとバックエンドの結合

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from '../../products'
+import { ProductService } from '../shared/product.service';
 
 
 @Component({
@@ -10,10 +10,40 @@ import { products } from '../../products'
 export class ProductListComponent implements OnInit {
   products: any;
   
-  constructor() { }
+  constructor( private productService: ProductService ) { }
 
   ngOnInit(): void {
-    this.products = products
+    // this.products = this.productService.getProducts()
+
+    const productsObservable = this.productService.getProducts()
+    productsObservable.subscribe(
+      (data) => { 
+        this.products = data
+      },
+      (err) => { console.error('次のエラーが発生しました: ' + err) }
+    )
+
+    // const observable = new Observable(subscriber => {
+    //   subscriber.next(1);
+    //   subscriber.next(2);
+    //   subscriber.error('エラー発生');
+    //   setTimeout(() => {
+    //     subscriber.next(4);
+    //     subscriber.complete();
+    //   }, 1000);
+    // });
+
+
+    // console.log('subscribe前');
+
+    // observable.subscribe({
+    //   next(data) { console.log('次のデータが出力されました:' + data); },
+    //   error(err) { console.error('次のエラーが発生しました:' + err); },
+    //   complete() { console.log('完了しました！'); }
+    // });
+    // console.log('subscribeから抜けました');
+
+
   }
 
 }
